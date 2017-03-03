98422a7b311c921310b877d303ef5965a5374bed
sunny <sunny@sunnydeMacBook-Air.local>
2017-03-04 06:51:53 +0800

    update info

--- src/component.vue
+++ src/component.vue
@@ -4,8 +4,10 @@
       <div class="calendar-btn calendar-btn-today" @click="focusToday()">今天</div>
       <div>
         <i class="calendar-prev" @click="prevMonth()"> &lt; </i>
-        <span class="calendar-curr-year">{{this.year}}</span> 年
-        <span class="calendar-curr-month">{{this.month + 1}}</span> 月
+        <div id="date-select-btn" data-options='{"type":"month"}'>
+          <span class="calendar-curr-year">{{this.year}}</span> 年
+          <span class="calendar-curr-month">{{this.month + 1}}</span> 月
+        </div>
         <i class="calendar-next" @click="nextMonth()"> &gt; </i>
       </div>
       <div>
@@ -16,15 +18,17 @@
       <div class="calendar-weeks">
         <div class="calendar-week" v-for="item in weeks">{{item}}</div>
       </div>
-      <div class="calendar-days">
-        <div class="calendar-days-line" v-for="(line, l) in days">
+      <div class="calendar-days" :class="{'week-mode': weekShowFlag}">
+        <div class="calendar-days-line" :class="{in: weekShowFlag==l}" v-for="(line, l) in days">
           <div class="calendar-day" 
             :class="{'calendar-day-dis': item.disabled, 'calendar-day-today': item.today,'calendar-day-selected': item.selected, 'calendar-day-plan-delivery': item.plan, 'calendar-day-stop-delivery': item.stop, 'calendar-day-farmer-delivery': item.farmer}"
             v-for="(item, i) in line" @click="handleDayClick(l, i)">
             {{item.day | zero}}
-            <span class="calendar-today-tips" >今天</span>
-            <i class="fa fa-star calendar-star"></i>
             <i class="mui-icon mui-icon-location calendar-location-arrow"></i>
+            <div class="calendar-today-tips-wrap">
+              <span class="calendar-today-tips" >今天</span>
+              <i class="fa fa-star calendar-star"></i>
+            </div>
           </div>
         </div>
       </div>
@@ -61,11 +65,11 @@
         type: String,
         default: 'date'
       },
-      daySelect: Function,
+      handleDaySelect: Function,
+      dateSelected: Object, //外部传入的当前选择的日期
       value: '',
       show: {
         type: Boolean,
-        default: false
       },
       weeks: {
         type: Array,
@@ -104,6 +108,8 @@
         month: now.getMonth(),
         day: now.getDate(),
         days: [],
+        today: new Date(),
+        weekShowFlag: null //上下滑动时显示哪一列的标志位
         /*activityModal: {
           show: true,
           startDate: dateFormat(now, 'isoDate'),
@@ -122,7 +128,14 @@
     mounted() {
       this.init()
     },
-    watch: {},
+    watch: {
+      dateSelected(val) {
+        if (val.y && val.M) {
+          this.render(val.y, val.M - 1)
+          this.triggerPropsDaySelect()
+        }
+      }
+    },
     methods: {
       init() {
         if (!this.value) {
@@ -131,6 +144,8 @@
           this.month = today.getMonth()
           this.render(this.year, this.month)
         }
+        this.handleSlidingAround()
+        this.handleSlideUpDown()
       },
       render(y, m) {
         let currMonthFirstDay = new Date(y, m, 1).getDay(), //本月第一天是周几
@@ -138,7 +153,7 @@
           lastMonthDays = new Date(y, m, 0).getDate(), //上个月总天数
           line = 0,
           temp = [],
-          today = new Date()
+          today = this.today
         // 渲染当前月日期
         for (let i = 1; i <= currMonthDays; i++) {
           let i_day = new Date(y, m, i).getDay()
@@ -168,11 +183,22 @@
               day: i
             }).format(this.dateParseFormat)
 
-          if (today.getDate() === i) {
-            _temp_day.today = true
+
+
+          //由父组件手动传递选择日期,只有在父组件没有值的情况下 才默认选择今天
+          if (this.dateSelected.str) {
+            if (this.dateSelected.d == i || (!this.dateSelected.d && this.day == i)) {
+              _temp_day.selected = true
+            }
           } else {
-            _temp_day.selected = false
+            if (this.day == i) {
+              _temp_day.selected = true
+              if (this.year === today.getFullYear() && this.month === today.getMonth()) {
+                _temp_day.today = true
+              }
+            }
           }
+
 
           if (this.planDelivery.indexOf(_temp_day_format) > -1) {
             _temp_day.plan = true
@@ -203,11 +229,78 @@
         this.year = new Date(y, m).getFullYear()
         this.month = new Date(y, m).getMonth()
       },
+      handleSlidingAround() {
+        var $target = document.querySelector('.calendar-body')
+        var start, end, deltax, deltay, self = this
+        $target.addEventListener('touchstart', function(e) {
+          start = {
+            x: e.touches[0].pageX,
+            y: e.touches[0].pageY
+          }
+        })
+        $target.addEventListener('touchmove', function(e) {
+          end = {
+            x: e.touches[0].pageX,
+            y: e.touches[0].pageY
+          }
+          deltax = end.x - start.x
+          deltay = end.y - start.y
+        })
+        $target.addEventListener('touchend', function(e) {
+          if (deltax > 100 && (deltay >= 50 || deltay <= 50)) {
+            self.nextMonth()
+          } else if (deltax < -100 && (deltay >= 50 || deltay <= 50)) {
+            self.prevMonth()
+          }
+        })
+      },
+      handleSlideUpDown() {
+        var $target = document.querySelector('.calendar-bottom-content')
+        var start, end, deltax, deltay, self = this
+        $target.addEventListener('touchstart', function(e) {
+          start = {
+            x: e.touches[0].pageX,
+            y: e.touches[0].pageY
+          }
+        })
+        $target.addEventListener('touchmove', function(e) {
+          end = {
+            x: e.touches[0].pageX,
+            y: e.touches[0].pageY
+          }
+          deltax = end.x - start.x
+          deltay = end.y - start.y
+        })
+        $target.addEventListener('touchend', function(e) {
+          if (deltay > 100 && (deltax >= 50 || deltax <= 50)) {
+            self.changeToMonth()
+          } else if (deltay < -100 && (deltax >= 50 || deltax <= 50)) {
+            self.changeToWeek()
+          }
+        })
+      },
+      // 伸展为月日历
+      changeToMonth() {
+        this.weekShowFlag = null
+      },
+      // 缩进为周日历
+      changeToWeek() {
+        this.days.forEach((line, l) => {
+          line.forEach(day => {
+            if (day.selected == true) {
+              this.weekShowFlag = l
+              return false
+            }
+          })
+        })
+      },
       prevMonth() {
         this.render(this.year, --this.month)
+        this.triggerPropsDaySelect()
       },
       nextMonth() {
         this.render(this.year, ++this.month)
+        this.triggerPropsDaySelect()
       },
       focusToday() {
         this.days.forEach((line) => {
@@ -215,6 +308,8 @@
             day.selected = false
           })
         })
+        this.day = this.today.getDate()
+        this.render(this.today.getFullYear(), this.today.getMonth())
       },
       handleDayClick(l, i) {
         this.days.forEach((line) => {
@@ -226,14 +321,20 @@
         let item = this.days[l][i]
         let day = item.day
         if (!item.disabled) {
-          if (item.selected) {
-            item.selected = false
-          } else {
+          if (item.selected) {} else {
             item.selected = true
             this.day = day
           }
         }
-        this.daySelect(moment({y:this.year,M: this.month, d: this.day}).format(this.dateParseFormat))
+        this.triggerPropsDaySelect()
+      },
+      //改变年月日都要重新请求当天配送计划
+      triggerPropsDaySelect() {
+        this.handleDaySelect(moment({
+          y: this.year,
+          M: this.month,
+          d: this.day
+        }).format(this.dateParseFormat))
       },
       addActivity() {
         this.activityModal.show = true
@@ -271,6 +372,10 @@
     display: none;
   }
   
+  .inline-block {
+    display: inline-block;
+  }
+  
   .font-12 {
     font-size: 12px;
   }
@@ -291,6 +396,10 @@
     position: relative;
   }
   
+  .text-right {
+    text-align: right;
+  }
+  
   .calendar-wrapper {
     background: #f8f8f8;
     box-shadow: 2px 2px 5px rgba(0, 0, 0, .1);
@@ -301,6 +410,9 @@
       padding: 10px 0;
       color: #fff;
       background: @base;
+      #date-select-btn {
+        .inline-block;
+      }
       .calendar-btn {
         .absolute;
         left: 15px;
@@ -340,12 +452,12 @@
           display: flex;
           justify-content: space-around;
           padding: 5px 5px 0 5px;
-          ;
           .calendar-day {
             padding: 0 6px 6px 6px;
             height: 34px;
             flex: 1;
             box-shadow: 0 0 2px rgba(0, 0, 0, .3);
+            border: 1px solid transparent;
             margin-right: 5px;
             background: #fff;
             font-size: 12px;
@@ -358,14 +470,12 @@
               background: #eee;
             }
             &.calendar-day-today {
-              .border-1-red;
-              .calendar-today-tips {
-                .block;
+              .calendar-today-tips-wrap .calendar-today-tips {
+                .inline-block;
               }
             }
             &.calendar-day-selected {
-              background: #383A3F;
-              color: @white;
+              .border-1-red;
             }
             &.calendar-day-plan-delivery {
               color: @red;
@@ -383,10 +493,7 @@
             }
             &.calendar-day-farmer-delivery {
               .calendar-star {
-                .block;
-                .absolute;
-                right: 6px;
-                bottom: 4px;
+                .inline-block;
                 color: @base;
               }
             }
@@ -394,14 +501,24 @@
             .calendar-location-arrow {
               .none;
             }
-            .calendar-today-tips {
-              .none;
-              .font-9;
-              .absolute;
-              right: 6px;
-              bottom: 4px;
-              line-height: normal;
-              color: @red;
+            .calendar-today-tips-wrap {
+              margin-top: -3px;
+              line-height: 12px;
+              .text-right;
+              .calendar-today-tips {
+                .none;
+                .font-9;
+                line-height: normal;
+                color: @red;
+              }
+            }
+          }
+        }
+        &.week-mode {
+          .calendar-days-line {
+            .none;
+            &.in {
+              display: flex;
             }
           }
         }
